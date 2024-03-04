c1b1b0bcacb6b9d390a8e8fd895d7ceed7694cec
hafizulf <hafizulf68@gmail.com>
2024-03-04 11:21:00 +0700

    feat: fetch employees data

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,14 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-    public function index() : JsonResponse {
+    public function index(Request $request) : JsonResponse {
+        $limit = $request->input('limit', 10);
+        $page = $request->input('page', 1);
+        $employees = Employee::paginate($limit, ['*'], 'page', $page);
+
         $data = [
-            'message' => 'pong'
+            'status' => 200,
+            'message' => 'Employees fetched',
+            'data' => $employees->items(),
+            'pagination' => [
+                'current_page' => $employees->currentPage(),
+                'first_page_url' => $employees->url(1),
+                'from' => $employees->firstItem(),
+                'last_page' => $employees->lastPage(),
+                'last_page_url' => $employees->url($employees->lastPage()),
+                'next_page_url' => $employees->nextPageUrl(),
+                'path' => $employees->path(),
+                'per_page' => $employees->perPage(),
+                'prev_page_url' => $employees->previousPageUrl(),
+                'to' => $employees->lastItem(),
+                'total' => $employees->total(),
+            ],
         ];
 
         return response()->json($data);
--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class employee extends Model
+class Employee extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
 
     /**
     * fillable
